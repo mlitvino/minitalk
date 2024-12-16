e14b4b98de76d7abce7e4678ac379153248758cb
Mykhailo Litvinov <mlitvino@student.hive.fi>
2024-12-16 17:08:43 +0200

    new project

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/16 12:26:51 by mlitvino          #+#    #+#             */
+/*   Updated: 2024/12/16 15:14:00 by mlitvino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include "libft.h"
+# include <signal.h>
+# include <sys/types.h>
+
+
+
+#include <stdio.h>
+
+#endif
