40bd8c3fd246ac5d2a12504bb0fe57b156c6e9d9
Mykhailo Litvinov <mlitvino@student.hive.fi>
2024-12-30 13:03:06 +0200

    header update

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: mlitvino <mlitvino@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 12:26:51 by mlitvino          #+#    #+#             */
-/*   Updated: 2024/12/30 12:21:33 by mlitvino         ###   ########.fr       */
+/*   Updated: 2024/12/30 13:02:09 by mlitvino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,15 @@
 # include <sys/types.h>
 # include "libft.h"
 
-void	send_pid(int *serv_pid, int *clnt_pid);
-void	send_str(int *serv_pid, char *str, int *len);
-void	send_size(int *serv_pid, int *len);
+void	send_pid(pid_t *serv_pid, pid_t *clnt_pid);
+void	send_len(pid_t *serv_pid, int *len);
+void	send_str(pid_t *serv_pid, char *str, int *len);
+
+void	get_clnt_pid(long int *bitlen, pid_t *client_pid, int sig);
+char	*get_len(long int *bitlen, int *len, char *str, int sig);
+char	*get_bit(char *str, long int *bitlen, int sig);
+
+void	sig_handler(int sig);
 
 #endif
+
